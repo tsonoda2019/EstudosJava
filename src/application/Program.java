56fc66ce7a0b1fba65893d57b20adb71880c80e6
Thiago Sonoda <thiago.sonoda2012@gmail.com>
2019-08-17 23:03:09 -0300

    Retirado mensagem de bom dia

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
 		System.out.println(sdf.format(d));
@@ -18,8 +18,7 @@ public class Program {
 		cal.add(Calendar.MINUTE, 4);
 		d = cal.getTime();
 		System.out.println(sdf.format(d));
-		
-		System.out.println("BOM DIA");
+
 	}
 
 }
